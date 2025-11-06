c57be890e8bbbdc2702fc87349d9b28f2bf55d71
Emre Acarsoy <ea616@exeter.ac.uk>
2025-11-06 23:46:10 +0000

    Improved script :)

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-DOTFILES_REPO="git@github.com:AtlasICL/dotfiles"
+#DOTFILES_REPO="git@github.com:AtlasICL/dotfiles"
 
-info()  { printf "\n[INFO] %s\n" "$*"; }
+# Set up colors for printing.
+INFO_COLOR="\033[94m"     # blue
+SUCCESS_COLOR="\033[35m"  # purple
+ERROR_COLOR="\033[31m"    # red
+RESET="\033[0m"
+
+# Define console log functions.
+info()  { printf "\n${INFO_COLOR}[INFO] %s${RESET}\n" "$*"; }
+success() { printf "\n${SUCCESS_COLOR}[OK  ] %s${RESET}\n" "$*"; }
+error() { printf "\n${ERROR_COLOR}[ERR ] %s${RESET}\n" "$*"; }
 
 if ! command -v apt >/dev/null 2>&1; then
   error "This script is for Debian/Ubuntu (apt-based) systems."
@@ -15,27 +24,26 @@ if ! sudo -v 2>/dev/null; then
 fi
 
 info "Updating package lists..."
-sudo apt update 
+sudo apt-get update > /dev/null 
 
 info "Upgrading packages..."
-sudo apt upgrade -y
+sudo apt-get upgrade -y > /dev/null
 
 info "Installing git..."
-sudo apt install -y git 
+sudo apt-get install -y git > /dev/null 
 
 info "Installing compilers..."
-sudo apt install -y build-essential
+sudo apt-get install -y build-essential > /dev/null
 
-info "Getting dotfiles..."
-git clone "$DOTFILES_REPO" ~
+#info "Getting dotfiles..."
+#git clone "$DOTFILES_REPO" ~
 
 info "Linking dotfiles into home directory..."
-cd ~
-cp ./dotfiles/.bashrc ~
-cp ./dotfiles/.bash_aliases ~
-cp ./dotfiles/.nanorc ~
-cp ./dotfiles/.gitconfig ~
+cp ~/dotfiles/.bashrc ~
+cp ~/dotfiles/.bash_aliases ~
+cp ~/dotfiles/.nanorc ~
+cp ~/dotfiles/.gitconfig ~
 
 source .bashrc
 
-info "Done."
+success "Done."
