f78a924592737867eef973889bf1eb3f8d3359a1
Emre Acarsoy <ea616@exeter.ac.uk>
2025-11-12 02:11:35 +0000

    big improvements

--- setup.sh
+++ setup.sh
@@ -6,13 +6,27 @@ set -euo pipefail
 INFO_COLOR="\033[92m"     # green
 SUCCESS_COLOR="\033[35m"  # purple
 ERROR_COLOR="\033[31m"    # red
+BAKINFO_COLOR="\033[94m"  # blue
 RESET="\033[0m"
 
 # Define console log functions.
 info()  { printf "\n${INFO_COLOR}[INFO] %s${RESET}\n" "$*"; }
+bakinfo() { printf "\n${BAKINFO_COLOR}[INFO] %s${RESET}\n" "$*"; }
 success() { printf "\n${SUCCESS_COLOR}[OK  ] %s${RESET}\n" "$*"; }
-warn() { printf "\n${ERROR_COLOR}[ERR ] %s${RESET}\n" "$*"; }
+warn() { printf "\n${ERROR_COLOR}[WARN] %s${RESET}\n" "$*"; }
 error() { printf "\n${ERROR_COLOR}[ERR ] %s${RESET}\n" "$*"; }
+
+# Function to create time-stamped backups of the files this script might overwrite.
+backup() {
+  local target="$1"
+  if [ -e "$target" ]; then
+    local ts="$(date +%Y%m%d-%H%M%S)"
+    local backup_path="${target}.bak.${ts}"
+    bakinfo "Backing up $target -> $backup_path"
+    cp -a -- "$target" "$backup_path"
+    bakinfo "Backup successful"
+  fi
+}
 
 if ! command -v apt-get >/dev/null 2>&1; then
   error "This script is for Debian/Ubuntu (apt-based) systems."
@@ -24,15 +38,10 @@ if ! sudo -v 2>/dev/null; then
   exit 1
 fi
 
-# DOTFILES_REPO="https://github.com/AtlasICL/dotfiles"
-
-# if [ ! -d ~/dotfiles/.git ]; then
-#   info "Cloning dotfiles..."
-#   git clone "$DOTFILES_REPO" ~/dotfiles
-# else
-#   info "dotfiles directory already exists at: ~/dotfiles. Pulling updates from GitHub..."
-#   git -C "~/dotfiles" pull --ff-only || warn "fast-forwards failed: skipping auto-pull."
-# fi
+if [ ! -d "${HOME}/dotfiles" ]; then
+  error "Missing ${HOME}/dotfiles directory. Clone dotfiles repo."
+  exit 1
+fi
 
 info "Updating packages..."
 sudo apt-get update > /dev/null 
@@ -50,14 +59,23 @@ sudo apt-get install -y fd-find > /dev/null
 sudo apt-get install -y ripgrep > /dev/null
 sudo apt-get install -y fzf > /dev/null
 
+info "Backing up current configs..."
+backup "${HOME}/.bashrc"
+backup "${HOME}/.bash_aliases"
+backup "${HOME}/.nanorc"
+backup "${HOME}/.gitconfig"
+
 info "Linking dotfiles into home directory..."
-cp ~/dotfiles/.bashrc ~
-cp ~/dotfiles/.bash_aliases ~
-cp ~/dotfiles/.nanorc ~
-cp ~/dotfiles/.gitconfig ~
+cp "${HOME}/dotfiles/.bashrc" "${HOME}"
+cp "${HOME}/dotfiles/.bash_aliases" "${HOME}"
+cp "${HOME}/dotfiles/.nanorc" "${HOME}"
+cp "${HOME}/dotfiles/.gitconfig" "${HOME}"
+
+info "Backing up neovim config..."
+backup "${HOME}/.config/nvim"
 
 info "Linking neovim config files..."
-mkdir -p ~/.config  # Create .config directory if necessary
-cp -r ~/dotfiles/nvim ~/.config/nvim
+mkdir -p "${HOME}/.config"  # Create .config directory if necessary
+cp -r "${HOME}/dotfiles/nvim" "${HOME}/.config/nvim"
 
 success "Done. You should restart your shell."
