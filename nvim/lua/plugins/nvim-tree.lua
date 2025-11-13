42fa38e5daa9db9c98c6aac60337c519ec01a6ce
Emre Acarsoy <ea616@exeter.ac.uk>
2025-11-13 14:46:50 +0000

    nvim config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,22 +1,60 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = {
-    "nvim-tree/nvim-web-devicons", -- Optional: for file icons
+    "nvim-tree/nvim-web-devicons", -- File icons
   },
   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
   keys = {
     { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file explorer" },
   },
   config = function()
+    -- Disable netrw (Neovim's default file explorer)
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
     require("nvim-tree").setup({
+      sort = {
+        sorter = "case_sensitive",
+      },
       view = {
-        width = 30,
+        width = 35,
+        side = "left",
       },
       renderer = {
         group_empty = true,
+        highlight_git = true,
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              arrow_closed = "",
+              arrow_open = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
       },
       filters = {
         dotfiles = false,
+      },
+      git = {
+        enable = true,
+        ignore = false,
       },
     })
   end,
