fc65590fed1c3813937fe8526e8c4de0d0bcbd69
Emre Acarsoy <emre@cloudflare.com>
2026-07-06 12:36:21 +0100

    chore: move optional language installs to own folder for cleanup

--- install_whatdidi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-tmp=$(mktemp -d) && curl -sLo "$tmp/whatdidi" https://raw.githubusercontent.com/AtlasICL/whatdidi/main/whatdidi && curl -sLo "$tmp/install" https://raw.githubusercontent.com/AtlasICL/whatdidi/main/install && bash "$tmp/install" && rm -rf "$tmp"
